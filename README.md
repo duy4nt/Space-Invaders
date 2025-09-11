7c1ac5230473d78486de5b0191ffdb926d671ee1
duy4nt <singhduyant@gmail.com>
2025-09-11 14:59:38 +0000

    Gettting the barebone of the game ready

---
+++ README.md
@@ -0,0 +1,9 @@
+# Space Invaders
+Why I chose to make a game you ask? cause I love programming and the best way to get better at programming is to make a game(and yes that's true, to some extent) and why this game? cause this was one of the first games I used to play.
+Enough of background story, now lets get to the game plan. I am not making a direct copy of the original game. Its sort of different.
+## Plans
+The hero sprite will spawn in the middle bottom of the screen; and yes I chose the retro themed resolution cause why not.
+The enemy sprite will spawn at the top and move towards the hero sprite in a linear path, which would pass through the position of the hero sprite at the time of dying of the previous sprite.
+There will be 3 levels of difficulty. In the easy difficutlty there will be one sprite, on the medium difficulty, there will be 3 sprite on the screen, at one time, and on the hard level, there will be random number of enemy sprites on the screen at a time, i havent thought about a maximum number of sprites.
+The enemy sprite can come from any side, but thats a topic for some other time, for not the enemy sprite will spawn from the top poisiotn and random x poisiton.
+
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 use bevy::prelude::*;
+use bevy::window::{Window, WindowPlugin};
+use rand::prelude::*;
 
 const BOUNDS: Vec2 = Vec2::new(1200.0, 640.0);
 
+const WINDOW_HEIGHT: f32 = 480.0;
+const WINDOW_WIDTH: f32 = 640.0;
+
+const SPRITE_WIDTH: f32 = 60.0;
+const SPRITE_HEIGHT: f32 = 30.0;
+
+#[derive(Resources)]
+struct GameRng(StdRng);
+
+struct Player{
+    name: String,
+    health: f32,
+}
+
+#[derive(Resources)]
+struct SpawnTimer {
+    timer: Timer,
+    current_interval: f32,
+    decrease_Rate: f32,
+}
+
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin{
+            primary_window: Some(Window {
+                title: "Space Invaders".to_string(),
+                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                resizable: false,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
         .insert_resource(Time::<Fixed>::from_hz(60.0))
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, (player_movement_system,),)
@@ -14,7 +46,9 @@ fn main() {
 #[derive(Component)]
 struct Player {
     movement_speed: f32,
+    rotation_speed: f32,
 }
+
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
@@ -37,10 +71,16 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     commands.spawn((Sprite::from_image(
         asset_server.load("player.png")),
-        Transform::from_xyz(0.0, 0.0, 0.0),
+        Transform::from_xyz(0.0,-( WINDOW_HEIGHT/2.0) + SPRITE_HEIGHT, 1.0),
         Player {
             movement_speed: 300.0,
+            rotation_speed: f32::to_radians(360.0)
         },
+    ));
+
+    commands.spawn((Sprite::from_image(
+        asset_server.load("yellow.png")),
+        Transform::from_xyz(0.0, 0.0, 0.0),
     ));
 }
 
@@ -50,25 +90,25 @@ fn player_movement_system(
     mut query: Query<(&mut Transform,&Player), With<Player>>,
 ) {
     for (mut player_transform, player) in query.iter_mut() {
-    let mut direction = Vec3::ZERO;
+        let mut direction = Vec3::ZERO;
 
-    if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        direction.x -= 1.0;
-    } 
-    if keyboard_input.pressed(KeyCode::ArrowRight) {
-        direction.x += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowUp) {
-        direction.y += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowDown) {
-        direction.y -= 1.0;
-    }
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            direction.x -= 1.0;
+        } 
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            direction.x += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            direction.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            direction.y -= 1.0;
+        }
 
-    if direction != Vec3::ZERO {
+        if direction != Vec3::ZERO {
             direction = direction.normalize();
-    }
+        }
     
-    player_transform.translation += direction * player.movement_speed * time.delta_secs();
-}
+        player_transform.translation += direction * player.movement_speed * time.delta_secs();
+    }
 }
